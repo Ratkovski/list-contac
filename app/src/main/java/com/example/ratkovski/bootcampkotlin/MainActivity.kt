898b845447851f409c28b950c98f2bae4e1ef415
Ratkovski <drezcry@hotmail.com>
2021-03-11 22:47:50 -0300

    .

--- app/src/main/java/com/example/ratkovski/bootcampkotlin/MainActivity.kt
+++ app/src/main/java/com/example/ratkovski/bootcampkotlin/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -23,47 +25,58 @@ class MainActivity : AppCompatActivity() {
         bindView()
         updateList()
     }
+
     private fun bindView() {
         rvList.adapter = adapter
-        rvList.layoutManager= LinearLayoutManager(this)
+        rvList.layoutManager = LinearLayoutManager(this)
 
     }
-    private fun updateList(){
+
+    private fun updateList() {
         //lista mokada lista fake
         adapter.updateList(
-            arrayListOf(
-                    Contact(
-                            name = "Ratkovski",
-                            phone = "(99)9000-0000",
-                            photo = "img.png"
-                    ),
-                    Contact(
-                            name = "Ratkovski",
-                            phone = "(99)9000-0000",
-                            photo = "img.png"
-                    )
-            )
-
+                arrayListOf(
+                        Contact(
+                                name = "Ratkovski",
+                                phone = "(99)9000-0000",
+                                photo = "img.png"
+                        ),
+                        Contact(
+                                name = "Ratkovski",
+                                phone = "(99)9000-0000",
+                                photo = "img.png"
+                        )
                 )
-        }
-//mostrar menu
+
+        )
+    }
+
+    //mostrar menu
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater: MenuInflater = menuInflater
         inflater.inflate(R.menu.menu, menu)
         return true
     }
 
-    private fun
-    
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+
+    }
+
     //capturar click dos items do menu
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
-            R.id.item_menu_1=>{
-
+            R.id.item_menu_1 -> {
+                showToast("item de menu 1")
+                true
             }
-            R.id.item_menu_2=>{
-
+            R.id.item_menu_2 -> {
+                showToast("item de menu 2")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
-            else => super.onOptionsItemSelected(item)
+
     }
-    }
+}
